a32f154542cab74e2eaa3959d507a04ada9a395b
Allan Renucci <allan.renucci@gmail.com>
2018-04-23 21:09:30 +0200

    Comment out macros

--- shared/src/main/scala/minitest/api/SourceLocation.scala
+++ shared/src/main/scala/minitest/api/SourceLocation.scala
@@ -29,31 +29,32 @@ final case class SourceLocation(
 
 object SourceLocation {
   implicit def fromContext: SourceLocation =
-    macro Macros.fromContext
+    SourceLocation(None, None, 0)
+  //   macro Macros.fromContext
 
-  @macrocompat.bundle class Macros(val c: whitebox.Context) {
-    import c.universe._
+  // @macrocompat.bundle class Macros(val c: whitebox.Context) {
+  //   import c.universe._
 
-    def fromContext: Tree = {
-      val (fileNameExpr, pathExpr, lineExpr) = getSourceLocation
-      val SourceLocationSym = symbolOf[SourceLocation].companion
-      q"""$SourceLocationSym($fileNameExpr, $pathExpr, $lineExpr)"""
-    }
+  //   def fromContext: Tree = {
+  //     val (fileNameExpr, pathExpr, lineExpr) = getSourceLocation
+  //     val SourceLocationSym = symbolOf[SourceLocation].companion
+  //     q"""$SourceLocationSym($fileNameExpr, $pathExpr, $lineExpr)"""
+  //   }
 
-    private def getSourceLocation = {
-      val line = c.Expr[Int](Literal(Constant(c.enclosingPosition.line)))
-      val file = STry(Option(c.enclosingPosition.source.file.file)).toOption.flatten
-      (wrapOption(file.map(_.getName)), wrapOption(file.map(_.getPath)), line)
-    }
+  //   private def getSourceLocation = {
+  //     val line = c.Expr[Int](Literal(Constant(c.enclosingPosition.line)))
+  //     val file = STry(Option(c.enclosingPosition.source.file.file)).toOption.flatten
+  //     (wrapOption(file.map(_.getName)), wrapOption(file.map(_.getPath)), line)
+  //   }
 
-    private def wrapOption[A](opt: Option[A]): c.Expr[Option[A]] =
-      c.Expr[Option[A]](
-        opt match {
-          case None =>
-            q"""_root_.scala.None"""
-          case Some(value) =>
-            val v = c.Expr[A](Literal(Constant(value)))
-            q"""_root_.scala.Option($v)"""
-        })
-  }
+  //   private def wrapOption[A](opt: Option[A]): c.Expr[Option[A]] =
+  //     c.Expr[Option[A]](
+  //       opt match {
+  //         case None =>
+  //           q"""_root_.scala.None"""
+  //         case Some(value) =>
+  //           val v = c.Expr[A](Literal(Constant(value)))
+  //           q"""_root_.scala.Option($v)"""
+  //       })
+  // }
 }
